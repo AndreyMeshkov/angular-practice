ddde7f71ad55c68da2fda0cd08e86b6e622ccf83
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-03-01 17:33:45 +0300

    feat: add event bindings

--- practice/src/app/app.component.ts
+++ practice/src/app/app.component.ts
@@ -13,13 +13,27 @@ export class AppComponent {
 
   obj = {a: 1, b: {c: 2}};
 
-   img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+   // img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+
+  inputValue = '';
 
   constructor() {
-    setTimeout(() => {
-      console.log('Timeout is over')
-      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/480px-Angular_full_color_logo.svg.png'
-    },5000)
+    // setTimeout(() => {
+    //   console.log('Timeout is over')
+    //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/480px-Angular_full_color_logo.svg.png'
+    // },5000)
+  }
+
+  onInput(event: KeyboardEvent) {
+    this.inputValue = (<HTMLInputElement>event.target).value
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str
+  }
+
+  onClick() {
+    console.log('Click')
   }
 
 }
