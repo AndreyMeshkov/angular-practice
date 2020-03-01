a3380dca579704491441b99764b56afb02511351
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-03-01 19:21:37 +0300

    feat: add ngModel

--- practice/src/app/app.component.ts
+++ practice/src/app/app.component.ts
@@ -7,33 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'Dynamic title';
-  number = 42;
-  arr = [1, 2, 3];
+  title = 'Initial'
 
-  obj = {a: 1, b: {c: 2}};
-
-   // img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
-
-  inputValue = '';
-
-  constructor() {
-    // setTimeout(() => {
-    //   console.log('Timeout is over')
-    //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/480px-Angular_full_color_logo.svg.png'
-    // },5000)
+  onInput(event: any) {
+    this.title = event.target.value
   }
-
-  onInput(event: KeyboardEvent) {
-    this.inputValue = (<HTMLInputElement>event.target).value
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str
-  }
-
-  onClick() {
-    console.log('Click')
-  }
-
 }
