e001288d9a4f379a82679f3ab6cab5545af28685
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-03-01 16:52:29 +0300

    feat: add interpolation

---
+++ practice/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  title = 'Dynamic title';
+  number = 42;
+  arr = [1, 2, 3];
+
+  obj = {a: 1, b: {c: 2}};
+
+   img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Timeout is over')
+      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/480px-Angular_full_color_logo.svg.png'
+    },5000)
+  }
+
+}
